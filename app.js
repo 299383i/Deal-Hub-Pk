78a6007589a83d9d92ecc25c528905a59756fdbc
299383i <121260073+299383i@users.noreply.github.com>
2026-06-26 16:25:01 +0500

    Update app.js

--- app.js
+++ app.js
@@ -140,3 +140,38 @@ if (searchBox) {
 // Start
 
 loadProducts();
+// ======================================
+// HERO AUTO SLIDER
+// ======================================
+
+const slides = document.querySelectorAll(".slide");
+
+if (slides.length > 0) {
+
+    let currentSlide = 0;
+
+    function showSlide(index) {
+
+        slides.forEach(slide => {
+            slide.classList.remove("active");
+        });
+
+        slides[index].classList.add("active");
+
+    }
+
+    showSlide(currentSlide);
+
+    setInterval(() => {
+
+        currentSlide++;
+
+        if (currentSlide >= slides.length) {
+            currentSlide = 0;
+        }
+
+        showSlide(currentSlide);
+
+    }, 5000);
+
+    }
